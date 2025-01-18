762dbf4ea486425beb9994a206589ae3b97a7a6c
wdg <e1138927@u.nus.edu>
2025-01-18 21:35:58 +0800

    Backend - Updated the backend rules.

--- server/rules.js
+++ server/rules.js
@@ -51,19 +51,19 @@ const rules = [
     id: 6,
     type: "wordInclusion",
     difficulty: 1,
-    description: "Your posts must include the phrase - \"I couldn’t have done without…\"",
-    words: ["i couldn’t have done without…"]
+    description: "Your posts must include the phrase - \"I couldn’t have done without\"",
+    words: ["i couldn’t have done without"]
   },
   {
     id: 7,
     type: "wordInclusion",
     difficulty: 1,
     description: "Your post must mention at least 1 of the buzzwords - \"inspired\", \"determined\", \"growth\", \"development\", \"efficient\".",
-    words: ["inspired", "determined", "growth", "development", "efficient"]
+    words: ["inspired", "determined", "growth", "development", "efficient", "perseverance", "respectful", "tech-savvy", "dedication", "milestone", "achievement."]
   },
   {
     id: 8,
-    type: "wordInclusion",
+    type: "characterInclusion",
     difficulty: 1,
     description: "Your post must include a special character.",
     words: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "[", "]", ":", ";", "'", '"', "<", ">", ".", "?", "/", "|", "~", "`"]
@@ -193,58 +193,52 @@ const rules = [
     description: "Your post must include one of the following companies - \"Grab\", \"DBS\", \"Singtel\".",
     words: ["grab", "dbs", "singtel"]
   },
+
   {
     id: 112,
-    type: "wordInclusion",
-    difficulty: 2,
-    description: "Your post must include one of the following companies - \"THALES\", \"Intel\", \"AMD\", \"Deloitte\".",
-    words: ["thales", "intel", "amd", "deloitte"]
-  },
-  {
-    id: 113,
     type: "wordInclusion",
     difficulty: 2,
     description: "Your post must include the sentence \"I have made significant personal growth with this experience\".",
     words: ["i have made significant personal growth with this experience"]
   },
   {
-    id: 114,
+    id: 113,
     type: "sentenceInclusion",
     difficulty: 2,
     description: "Your post must mention at least 2 of the buzzwords in a single sentence - dynamic, scalable, innovation, resilient, open-minded, ambitious, productivity, impact, opportunity, unity, community, teamwork, network, strategic, equality, digital transformation.",
     words: ["dynamic", "scalable", "innovation", "resilient", "open-minded", "ambitious", "productivity", "impact", "opportunity", "unity", "community", "teamwork", "network", "strategic", "equality", "digital transformation" ]
   },
   {
-    id: 115,
+    id: 114,
     type: "wordInclusion",
     difficulty: 2,
     description: "Your post must include one of the following acronyms and explain what it stands for - AGILE, KNY, SCRUM, DRY, DAC.",
     words: ["agile", "kny", "scrum", "dry", "dac"]
   },
   {
-    id: 116,
+    id: 115,
     type: "wordInclusion",
     difficulty: 2,
     description: "Your post must mention the founding father of Singapore.",
     words: ["lee kuan yew"]
-  },
+  }, 
   
   {
-    id: 117,
+    id: 116,
     type: "wordInclusion",
     difficulty: 2,
     description: "Your post must mention an English phrase that has the words \"journey\" and \"steps\".\nHint: It\'s a Chinese proverb.",
     words: ["a journey of a thousand miles begins with a single step"]
   },
   {
-    id: 118,
+    id: 117,
     type: "wordInclusion",
     difficulty: 2,
     description: "Your post must mention a European country.",
     words: ["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Georgia", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kazakhstan", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Monaco", "Montenegro", "Netherlands", "North Macedonia", "Norway", "Poland", "Portugal", "Romania", "Russia", "San Marino", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Türkiye", "Ukraine", "United Kingdom", "Vatican City"]
   }, 
   {
-    id: 119,
+    id: 118,
     type: "wordInclusion",
     difficulty: 2,
     description: "Your post must contain the anagram of 'sleuth'.",
@@ -252,7 +246,7 @@ const rules = [
 
   },
   {
-    id: 120,
+    id: 119,
     type: "wordInclusion",
     difficulty: 2,
     description: "Your post must contain the anagram of 'hither'. ",
@@ -260,26 +254,41 @@ const rules = [
 
   },
   {
+    id: 120,
+    type: "wordInclusion",
+    difficulty: 2,
+    description: "Your post must contain the anagram of 'verdin'.",
+    words: ["driven"]
+  },
+  {
     id: 121,
     type: "wordInclusion",
     difficulty: 2,
-    description: "Your post must contain the anagram of “verdin”",
-    words: ["driven"]
+    description: "Your post must contain the anagram of 'orwngt'.",
+    words: ["growth"]
   },
   {
     id: 122,
     type: "wordInclusion",
     difficulty: 2,
-    description: "Your post must contain the anagram of ”worngt",
-    words: ["growth"]
-  },
+    description: "Your post must contain the anagram of 'cussses'.",
+    words: ["success"]
+  }, 
   {
     id: 123,
     type: "wordInclusion",
     difficulty: 2,
-    description: "Your post must contain the anagram of cussses",
-    words: ["success"]
-  },
+    description: "Your post must contain the english word of the foreign word 'líder'",
+    hint: "It's a portuguese word.",
+    word: ["leader"]
+  }, 
+  {
+    id: 124,
+    type: "wordInclusion",
+    difficulty: 2,
+    description: "Your post must contain the word provided in this captcha.",
+    word: ["resilient"]
+  },  
 
 
 
@@ -299,6 +308,36 @@ const rules = [
     description: "Your post must mention an English phrase that has the words \"mother\" and \"failure\". \nHint: It\'s a Chinese proverb.",
     words: ["failure is the mother of success"]
   },
+  {
+    id: 203,
+    type: "wordInclusion",
+    difficulty: 3,
+    description: "Your post must mention South Africa's first democratically elected president ",
+    words: ["nelson mandela"]
+  },
+  {
+    id: 204,
+    type: "math",
+    difficulty: 3,
+    description: "Add the constant number of this differentation question - d/dx(3x^2)",
+    number: [6]
+  },
+
+  {
+    id: 205,
+    type: "math",
+    difficulty: 3,
+    description: "Add the constant number of this differentation question - d/dx(3x^2)",
+    number: [6]
+  }, 
+  {
+    id: 206,
+    type: "wordInclusion",
+    difficulty: 2,
+    description: "Your post must contain the english word of the foreign word 'motivatsiya'",
+    hint: "It's a russian word.",
+    word: ["motivation"]
+  }, 
   
 
 
